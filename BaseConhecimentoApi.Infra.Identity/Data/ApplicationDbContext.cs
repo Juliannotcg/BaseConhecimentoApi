8014a8b2f3def9b49729d1c3e90fa561ade1823e
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-09-10 21:58:21 -0300

    Criando as migrations

--- BaseConhecimentoApi.Infra.Identity/Data/ApplicationDbContext.cs
+++ BaseConhecimentoApi.Infra.Identity/Data/ApplicationDbContext.cs
@@ -9,6 +9,10 @@ namespace BaseConhecimentoApi.Infra.Identity.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext()
+        {
+
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
